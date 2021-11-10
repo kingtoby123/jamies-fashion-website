cbd2948eeafecd9dbd9304471723d605906535c0
Jodie Linn Shires <jodie@Jodies-MacBook-Pro.local>
2021-11-10 12:51:37 -0800

    fixing the shop page

--- src/components/pages/shop.js
+++ src/components/pages/shop.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import AboutMe from '../../../static/assets/img/about/AboutMe.jpg'
+
 
 export default class Shop extends Component {
     constructor(props) {
@@ -11,13 +13,211 @@ export default class Shop extends Component {
         return (
             <div className="shop-wrapper">
 
-                <div>
+                <div className="picture">
 
-                    <h3>
-                        shopping
 
-                    </h3>
-                </div>
+                    <div className="fashion-picture-one">
+                            <img src={AboutMe} alt="About Me Img" />
+                        
+                    </div>
+                    <div className="fashion-picture-two">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-three">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-four">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-five">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-six">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-seven">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-eight">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-nine">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-ten">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-eleven">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twelve">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirteen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-fourteen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-fifthteen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-sixteen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-seventeen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-eighteen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-nineteen">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twenty">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentyone">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentytwo">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentythree">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentyfour">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentyfive">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentysix">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentyseven">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentyeight">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-twentynine">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    
+                    <div className="fashion-picture-thirty">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtyone">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtytwo">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtythree">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtyfour">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtyfive">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtyysix">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtyyseven">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtyeight">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-thirtynine">
+                            <img src={AboutMe} alt="About Me Img" />
+
+
+                    </div>
+                    <div className="fashion-picture-forty">
+                            <img src={AboutMe} alt="About Me Img" />
+
+                    </div>
+
+               </div>
+
                 
             </div>
         )
