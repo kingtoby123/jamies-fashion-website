de8154e3f7296febcb9fe312c36de930a12c0a3d
Toby English <tobyenglish132@gmail.com>
2021-11-14 07:03:04 -0800

    update

--- src/components/slider/home.js
+++ src/components/slider/home.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import AboutMe from '../../../static/assets/img/about/AboutMe.jpg'
 
-export default function SlideShow(props) {
 
-        const colors = ["#0088FE", "#00C49F", "#FFBB28"];
-    return (
-        <div className="Slide-show">
 
-        <div className="slideshow"></div>
-        <div className="slideShowSLider">
+export default function Slideshow() {
 
-            {colors.map((backgroundColor, index) => (
-            <div className="slide" key={index} style={{ backgroundColor }}/>))}
 
-        </div>
-        <div className="slider"></div>
-        
+  return (
+    <div class="slider">
+    <span id="slide-1"></span>
+    <span id="slide-2"></span>
+    <span id="slide-3"></span>
+    <div class="image-container">
+      <img src= {AboutMe} class="slide" width="500" height="300" />
+      <img src= {AboutMe} class="slide" width="500" height="300" />
+      <img src= {AboutMe} class="slide" width="500" height="300" />
     </div>
-    )
+    <div class="buttons">
+      <a href="#slide-1"></a>
+      <a href="#slide-2"></a>
+      <a href="#slide-3"></a>
+    </div>
+  </div>
+  );
 }
+  
